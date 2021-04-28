c5cea25c7330601d83d4f8807a29f5e9a8e87639
Ujjwal Tyagi <35771831+ujjwal159711@users.noreply.github.com>
2021-04-28 23:27:02 +0530

    Updated_RecordTracker.ts
    Updated record tracker ts file

--- RecordTracker.ts
+++ RecordTracker.ts
@@ -1,46 +1,104 @@
+import {Promise} from 'es6-promise';
+
 interface Client{
     sendMessage: (message: string) => void;
     onMessage: (callback: (message: string) => void) => void;
 }
 
-type RecordMap = { [recordName: string]: Client };
+type RecordMap = {                              //it is type of object
+    [recordName: string]: Client
+};              
 
 class RecordTracker {
+
     constructor (private recordMap: RecordMap) {}
-    public initialize(): void {
-    }
-    public printPingMessage(): void{
-        setInterval((x:string) => console.log(x), 10000, "Ping");
-    }
+
+    public initialize(): void {}
+
     public getRecordMap(): RecordMap {
         return {...this.recordMap};
     }
 }
 
-//-------------------------------------------------------------------------------------------------
+//=================================================================================================================
 
-function a1(msg:string):void{
-    //console.log("msg");
+//        -------------------Sending "ping" message and "pong" message using callback-------------------
+
+function send(msg:string):void{
+    console.log("Sending ping message each after 10 seconds :");
+    setInterval((x:string) => console.log(x), 10000, msg); 
 }
 
-function b1(fn:(msg:string)=>void):void{
-    fn("Hello There...!!!");
+function call(back:(msg:string)=>void):void {
+    console.log("Receiving pong message sing callback function:");
+    const replyMessage = "Pong";
+    back(replyMessage);
 }
 
-let ob1 : Client;
+call((msg) => {
+    console.log(msg);
+    console.log();
+});
+send("Ping");
 
-ob1 = {
-    sendMessage: a1,
-    onMessage: b1
+
+// call(replyCallBack);                    //   ------->    another way to call callback function
+// function replyCallBack(msg:string){
+//     console.log(msg); 
+// }
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+//         -----------------------Creating client type array for RecordMap------------------------
+
+let client1 : Client;
+let client2 : Client;
+client1 = {
+    sendMessage: send,
+    onMessage: call
+};
+client2 = {
+    sendMessage: send,
+    onMessage: call
 };
 
-let ob2 : RecordMap;
-ob2 = {
-    recordName : ob1
-};
+let arr:Client[]=[client1,client2];
+let recordM : RecordMap = {};
+recordM = {
+    recordName:client1
+}
+//console.log(arr);
 
-const obj = new RecordTracker(ob2);
-//console.log(obj.initialize());
-console.log(obj.getRecordMap());
-console.log("Printing ping message each after 10 seconds :");
-console.log(obj.printPingMessage());
+
+//-----------------------------------------------------------------------------------------------------------------
+
+//                     ---------------------- Creating object of class ------------------------
+
+const obj = new RecordTracker(recordM);
+//console.log(obj.getRecordMap());
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+//                 ------------------ Trying promise with ping and pong message --------------------
+
+
+const pro :Promise<string> = new Promise((resolve,reject) => {
+    setInterval(() => {resolve("ping");}, 10000);
+    setTimeout(()=>{ reject("pong"); } ,2000);
+});
+
+pro.then(data => {
+    ('');
+    ('');
+});
+
+
+//===============================================================================================================
+
+
+
+
+
+
