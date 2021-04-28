096869fc25f661f3e0faf2c7e17c88683d5376bc
Ujjwal Tyagi <35771831+ujjwal159711@users.noreply.github.com>
2021-04-28 23:30:05 +0530

    Updated_RecordTracker.js

--- RecordTracker.js
+++ RecordTracker.js
@@ -1,3 +1,4 @@
+"use strict";
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -9,39 +10,68 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+exports.__esModule = true;
+var es6_promise_1 = require("es6-promise");
 var RecordTracker = /** @class */ (function () {
     function RecordTracker(recordMap) {
         this.recordMap = recordMap;
     }
-    RecordTracker.prototype.initialize = function () {
-    };
-    RecordTracker.prototype.printPingMessage = function () {
-        //console.log("Priting ping message each after 10 seconds :");
-        setInterval(function (x) { return console.log(x); }, 10000, "Ping");
-    };
+    RecordTracker.prototype.initialize = function () { };
     RecordTracker.prototype.getRecordMap = function () {
         return __assign({}, this.recordMap);
     };
     return RecordTracker;
 }());
-//-------------------------------------------------------------------------------------------------
-function a1(msg) {
-    //console.log("msg");
+//=================================================================================================================
+//        -------------------Sending "ping" message and "pong" message using callback-------------------
+function send(msg) {
+    console.log("Sending ping message each after 10 seconds :");
+    setInterval(function (x) { return console.log(x); }, 10000, msg);
 }
-function b1(fn) {
-    fn("Hello There...!!!");
+function call(back) {
+    console.log("Receiving pong message sing callback function:");
+    var replyMessage = "Pong";
+    back(replyMessage);
 }
-var ob1;
-ob1 = {
-    sendMessage: a1,
-    onMessage: b1
+call(function (msg) {
+    console.log(msg);
+    console.log();
+});
+send("Ping");
+// call(replyCallBack);                    //   ------->    another way to call callback function
+// function replyCallBack(msg:string){
+//     console.log(msg); 
+// }
+//--------------------------------------------------------------------------------------------------------------------
+//         -----------------------Creating client type array for RecordMap------------------------
+var client1;
+var client2;
+client1 = {
+    sendMessage: send,
+    onMessage: call
 };
-var ob2;
-ob2 = {
-    recordName: ob1
+client2 = {
+    sendMessage: send,
+    onMessage: call
 };
-var obj = new RecordTracker(ob2);
-//console.log(obj.initialize());
-console.log(obj.getRecordMap());
-console.log("Printing ping message each after 10 seconds :");
-console.log(obj.printPingMessage());
+var arr = [client1, client2];
+var recordM = {};
+recordM = {
+    recordName: client1
+};
+//console.log(arr);
+//-----------------------------------------------------------------------------------------------------------------
+//                     ---------------------- Creating object of class ------------------------
+var obj = new RecordTracker(recordM);
+//console.log(obj.getRecordMap());
+//----------------------------------------------------------------------------------------------------------------
+//                 ------------------ Trying promise with ping and pong message -------------------
+var pro = new es6_promise_1.Promise(function (resolve, reject) {
+    setInterval(function () { resolve("ping"); }, 10000);
+    setTimeout(function () { reject("pong"); }, 2000);
+});
+pro.then(function (data) {
+    ('');
+    ('');
+});
+//===============================================================================================================
