9022685a4c3610cd12f66b37feec56c69f45ab91
Ujjwal Tyagi <35771831+ujjwal159711@users.noreply.github.com>
2021-05-03 09:29:53 +0530

    Update RecordTracker.js

--- RecordTracker.js
+++ RecordTracker.js
@@ -1,4 +1,5 @@
-"use strict";
+//import { Promise } from 'es6-promise';
+//global.Promise = require('es6-promise').Promise;
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -10,8 +11,42 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-exports.__esModule = true;
-var es6_promise_1 = require("es6-promise");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var RecordTracker = /** @class */ (function () {
     function RecordTracker(recordMap) {
         this.recordMap = recordMap;
@@ -66,12 +101,46 @@ var obj = new RecordTracker(recordM);
 //console.log(obj.getRecordMap());
 //----------------------------------------------------------------------------------------------------------------
 //                 ------------------ Trying promise with ping and pong message -------------------
-var pro = new es6_promise_1.Promise(function (resolve, reject) {
-    setInterval(function () { resolve("ping"); }, 10000);
-    setTimeout(function () { reject("pong"); }, 2000);
-});
-pro.then(function (data) {
-    ('');
-    ('');
-});
+// const pro = new Promise<string>((resolve,reject) => {
+//     setInterval(() => {resolve("ping");}, 10000);
+//     setTimeout(()=>{ reject("pong"); } ,2000);
+// });
+// pro.then(data => {
+//     ('');
+//     ('');
+// });
 //===============================================================================================================
+function findActiveClients(c, timeOut) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/, new Promise(function (resolve, reject) {
+                    setTimeout(function () { return resolve(false); }, timeOut);
+                    var pongMsg = function (msg) {
+                        if (msg === 'pong') {
+                            resolve(true);
+                        }
+                    };
+                    c.onMessage = pongMsg;
+                    c.sendMessage('ping');
+                })];
+        });
+    });
+}
+//-------------------------------------------------------------------------------
+// function activeClients(client,timeOut){
+//     return new Promise<boolean>((resolve,reject)  => {
+//         setTimeout(() => resolve(false),timeOut)
+//         const pongReceiver = ( msg ) => {
+//             if(msg === 'pong') {
+//                 resolve(true)
+//             }
+//         }
+//         client.onMessage = pongReceiver;
+//         client.sendMessage('ping');
+//     });
+//     //return pro;
+// }
+// async function result() {
+//     const pro = await activeClients();
+// }
+// console.log(activeClients(client1,10000));
